6a3551c411efc93729efadf014c5657d1346a9ed
Paluthyst <66975756+Paluthyst@users.noreply.github.com>
2020-10-05 18:15:06 -0400

    Add project files.

---
+++ TriCatch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TriCatch
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            try //try starts the catch with the thing you are trying to test for errors
+            {
+                int[] myNumbers = { 1, 5, 70 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch(Exception e) //in () you state the exception specifically or just do a blanked catch with Exception
+            // you can set the exception as a variable in this case 'e'. You can then print out the exception
+            {
+                Console.WriteLine("something went wrong?");
+                Console.WriteLine(e);
+            }
+            
+            
+        }
+    }
+}
